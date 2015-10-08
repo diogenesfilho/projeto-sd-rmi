3f3af51e3feba667a2cfdb0e0633522dabef7e78
diogenesfilho <diogeneslaertiussof@gmail.com>
2015-10-08 13:25:54 -0300

    Inicial

---
+++ src/tutorial/org/br/Constants.java
@@ -0,0 +1,15 @@
+package tutorial.org.br;
+
+public class Constants {
+ 
+  
+   public static final String LOCATION_HANOI = "HaNoi";   
+   public static final String LOCATION_TOKYO = "Tokyo";
+   public static final String LOCATION_CHICAGO = "Chicago";
+  
+   public static final String WEATHER_RAIN ="rain";
+    
+  
+   public static final String WEATHER_SUNNY ="sunny";
+ 
+}
